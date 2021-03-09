a2d2841b264b929b52c3b08461367860939d0fb3
DaryaWebDev <feral2015@mail.ru>
2021-03-09 19:59:00 +0300

    create: map in hotel

--- js/main.js
+++ js/main.js
@@ -12,3 +12,45 @@ const swiper = new Swiper(".swiper-container", {
     onlyInViewport: false,
   },
 });
+
+ymaps.ready(init);
+function init() {
+  // Создание карты.
+  var myMap = new ymaps.Map("map", {
+      // Координаты центра карты.
+      // Порядок по умолчанию: «широта, долгота».
+      // Чтобы не определять координаты центра карты вручную,
+      // воспользуйтесь инструментом Определение координат.
+      center: [7.838124988912487, 98.2988759307281],
+
+      // Уровень масштабирования. Допустимые значения:
+      // от 0 (весь мир) до 19.
+      zoom: 15,
+    }),
+    // Создаём макет содержимого.
+    MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+      '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+    ),
+    myPlacemark = new ymaps.Placemark(
+      myMap.getCenter(),
+      {
+        hintContent: "Собственный значок метки",
+        balloonContent: "This is your hotel",
+      },
+      {
+        // Опции.
+        // Необходимо указать данный тип макета.
+        iconLayout: "default#image",
+        // Своё изображение иконки метки.
+        iconImageHref: "../img/map-icon.jpg",
+        // Размеры метки.
+        iconImageSize: [40, 40],
+        // Смещение левого верхнего угла иконки относительно
+        // её "ножки" (точки привязки).
+        iconImageOffset: [-23, -28],
+      }
+    );
+     myMap.geoObjects
+        .add(myPlacemark)
+}
+
