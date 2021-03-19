c26d4a33d5b2bea87675b90316935e8be3d3a44d
DaryaWebDev <feral2015@mail.ru>
2021-03-19 22:15:46 +0300

    create: escape modal window

--- js/main.js
+++ js/main.js
@@ -81,7 +81,7 @@ menuButton.addEventListener("click", function () {
   var modalButton = $("[data-toggle=modal]");
   var closeModalButton = $(".modal__close");
   modalButton.on('click', openModal);
-  closeModalButton.on('click', closeModal)
+  closeModalButton.on('click', closeModal);
 
   function openModal() {
     var modalOverlay = $('.modal__overlay');
@@ -96,5 +96,13 @@ menuButton.addEventListener("click", function () {
     var modalDialog = $(".modal__dialog");
     modalOverlay.removeClass("modal__overlay--visible");
     modalDialog.removeClass("modal__dialog--visible");
-  }  
+  }
+  
+  $(document).on("keydown", function (e) {
+    if (e.keyCode == 27)
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.removeClass("modal__overlay--visible");
+    modalDialog.removeClass("modal__dialog--visible");
+  });
 });
