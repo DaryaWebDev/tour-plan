b6097d4aa5137fd2cc6db8bbb1ba438999a58df3
DaryaWebDev <feral2015@mail.ru>
2021-03-19 20:04:37 +0300

    create: modal window

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+$(document).ready(function () {
 const hotelSlider = new Swiper(".hotel-slider", {
   // Optional parameters
   loop: true,
@@ -75,4 +76,25 @@ menuButton.addEventListener("click", function () {
   document
     .querySelector(".navbar-bottom")
     .classList.toggle("navbar-bottom_visible");
+});
+  
+  var modalButton = $("[data-toggle=modal]");
+  var closeModalButton = $(".modal__close");
+  modalButton.on('click', openModal);
+  closeModalButton.on('click', closeModal)
+
+  function openModal() {
+    var modalOverlay = $('.modal__overlay');
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.addClass("modal__overlay--visible");
+    modalDialog.addClass("modal__dialog--visible");
+  }
+  
+  function closeModal(event) {
+    event.preventDefault();
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.removeClass("modal__overlay--visible");
+    modalDialog.removeClass("modal__dialog--visible");
+  }  
 });
