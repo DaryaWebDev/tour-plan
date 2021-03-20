9f3b3b1da5f429fd4ebb96ab1db856e980c682a4
DaryaWebDev <feral2015@mail.ru>
2021-03-21 00:25:27 +0300

    added: js

--- js/main.js
+++ js/main.js
@@ -1,108 +1,145 @@
 $(document).ready(function () {
-const hotelSlider = new Swiper(".hotel-slider", {
-  // Optional parameters
-  loop: true,
+  const hotelSlider = new Swiper(".hotel-slider", {
+    // Optional parameters
+    loop: true,
 
-  // Navigation arrows
-  navigation: {
-    nextEl: ".hotel-slider__button--next",
-    prevEl: ".hotel-slider__button--prev",
-  },
-  keyboard: {
-    enabled: true,
-    onlyInViewport: false,
-  },
-});
+    // Navigation arrows
+    navigation: {
+      nextEl: ".hotel-slider__button--next",
+      prevEl: ".hotel-slider__button--prev",
+    },
+    keyboard: {
+      enabled: true,
+      onlyInViewport: false,
+    },
+  });
 
-const reviewsSlider = new Swiper(".reviews-slider", {
-  // Optional parameters
-  loop: true,
+  const reviewsSlider = new Swiper(".reviews-slider", {
+    // Optional parameters
+    loop: true,
 
-  // Navigation arrows
-  navigation: {
-    nextEl: ".reviews-slider__button--next",
-    prevEl: ".reviews-slider__button--prev",
-  },
-  // keyboard: {
-  //   enabled: true,
-  //   onlyInViewport: false,
-  // },
-});
+    // Navigation arrows
+    navigation: {
+      nextEl: ".reviews-slider__button--next",
+      prevEl: ".reviews-slider__button--prev",
+    },
+    // keyboard: {
+    //   enabled: true,
+    //   onlyInViewport: false,
+    // },
+  });
 
-ymaps.ready(init);
-function init() {
-  // Создание карты.
-  var myMap = new ymaps.Map("map", {
-      // Координаты центра карты.
-      // Порядок по умолчанию: «широта, долгота».
-      // Чтобы не определять координаты центра карты вручную,
-      // воспользуйтесь инструментом Определение координат.
-      center: [7.838124988912487, 98.2988759307281],
+  ymaps.ready(init);
+  function init() {
+    // Создание карты.
+    var myMap = new ymaps.Map("map", {
+        // Координаты центра карты.
+        // Порядок по умолчанию: «широта, долгота».
+        // Чтобы не определять координаты центра карты вручную,
+        // воспользуйтесь инструментом Определение координат.
+        center: [7.838124988912487, 98.2988759307281],
 
-      // Уровень масштабирования. Допустимые значения:
-      // от 0 (весь мир) до 19.
-      zoom: 15,
-    }),
-    // Создаём макет содержимого.
-    MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
-      '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
-    ),
-    myPlacemark = new ymaps.Placemark(
-      myMap.getCenter(),
-      {
-        hintContent: "Собственный значок метки",
-        balloonContent: "This is your hotel",
-      },
-      {
-        // Опции.
-        // Необходимо указать данный тип макета.
-        iconLayout: "default#image",
-        // Своё изображение иконки метки.
-        iconImageHref: "../img/map-icon.jpg",
-        // Размеры метки.
-        iconImageSize: [40, 40],
-        // Смещение левого верхнего угла иконки относительно
-        // её "ножки" (точки привязки).
-        iconImageOffset: [-23, -28],
-      }
-    );
-  myMap.geoObjects.add(myPlacemark);
-}
+        // Уровень масштабирования. Допустимые значения:
+        // от 0 (весь мир) до 19.
+        zoom: 15,
+      }),
+      // Создаём макет содержимого.
+      MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+        '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+      ),
+      myPlacemark = new ymaps.Placemark(
+        myMap.getCenter(),
+        {
+          hintContent: "Собственный значок метки",
+          balloonContent: "This is your hotel",
+        },
+        {
+          // Опции.
+          // Необходимо указать данный тип макета.
+          iconLayout: "default#image",
+          // Своё изображение иконки метки.
+          iconImageHref: "../img/map-icon.jpg",
+          // Размеры метки.
+          iconImageSize: [40, 40],
+          // Смещение левого верхнего угла иконки относительно
+          // её "ножки" (точки привязки).
+          iconImageOffset: [-23, -28],
+        }
+      );
+    myMap.geoObjects.add(myPlacemark);
+  }
 
-var menuButton = document.querySelector(".menu-button"); //ищем этот класс
-menuButton.addEventListener("click", function () {
-  //прослушиватель событий
-  console.log("Клик");
-  document
-    .querySelector(".navbar-bottom")
-    .classList.toggle("navbar-bottom_visible");
-});
-  
-  var modalButton = $("[data-toggle=modal]");
-  var closeModalButton = $(".modal__close");
-  modalButton.on('click', openModal);
-  closeModalButton.on('click', closeModal);
+  var menuButton = document.querySelector(".menu-button"); //ищем этот класс
+  menuButton.addEventListener("click", function () {
+    //прослушиватель событий
+    document
+      .querySelector(".navbar-bottom")
+      .classList.toggle("navbar-bottom_visible");
+  });
 
-  function openModal() {
-    var modalOverlay = $('.modal__overlay');
+  var modalButton = $("[data-toggle=modal]"); //переменная - кнопка, с нее выходит модальное окно
+  var closeModalButton = $(".modal__close"); //привязка перем к "крестику"
+  modalButton.on("click", openModal); //при клике на кнопку выполнение ф-и openModal
+  closeModalButton.on("click", closeModal); //при клике на "крестик" выполнение ф-и closeModal
+
+  //добавляем класс visible
+  function openModal() { //открыть окно - применить класс visible к нашему фону и форме
+    var modalOverlay = $(".modal__overlay");
     var modalDialog = $(".modal__dialog");
+     document.body.style.overflow = "hidden";
+    // let scrollX = window.scrollX;
+    // let scrollY = window.scrollY;
+    // window.onscroll = function () {
+    //   window.scrollTo(scrollX, scrollY);
+    // };
     modalOverlay.addClass("modal__overlay--visible");
     modalDialog.addClass("modal__dialog--visible");
   }
-  
-  function closeModal(event) {
+
+  //удаляем класс visible
+  function closeModal(event) { //закрыть окно - удалить класс visible
     event.preventDefault();
     var modalOverlay = $(".modal__overlay");
     var modalDialog = $(".modal__dialog");
+    document.body.style.overflow = "auto";
     modalOverlay.removeClass("modal__overlay--visible");
     modalDialog.removeClass("modal__dialog--visible");
   }
-  
+
+  //закрытие окна кнопкой esc
   $(document).on("keydown", function (e) {
-    if (e.keyCode == 27)
-    var modalOverlay = $(".modal__overlay");
+    if (e.keyCode == 27) var modalOverlay = $(".modal__overlay");
     var modalDialog = $(".modal__dialog");
+    document.body.style.overflow = "auto";
     modalOverlay.removeClass("modal__overlay--visible");
     modalDialog.removeClass("modal__dialog--visible");
   });
+
+  $(document).mouseup(function (e) { // событие клика по веб-документу
+    var div = $('.modal__dialog'); // тут указываем класс элемента
+    if (!div.is(e.target) // если клик был не по нашему блоку
+      && div.has(e.target).length === 0) { // и не по его дочерним элементам
+      document.body.style.overflow = "auto";
+      var modalDialog = $(".modal__dialog");
+      modalDialog.removeClass("modal__dialog--visible");
+      var modalOverlay = $(".modal__overlay");
+      modalOverlay.removeClass("modal__overlay--visible");
+    }
+  });
+
+
+
+
+  // $(document) .mouseup(function (e) {
+  //   var container = $(".modal__dialog");
+  //   if (container.has(e.target).length === 0)
+  //     var modalDialog = $(".modal__dialog");
+  //   var modalOverlay = $(".modal__overlay");
+  //   modalDialog.removeClass("modal__dialog--visible");
+  //   modalOverlay.removeClass("modal__overlay--visible");
+  // });
+
+
+
+
 });
--- send.php
+++ send.php
@@ -6,10 +6,10 @@ require 'phpmailer/Exception.php';
 if(isset($_POST["subs"])){
     $email = $_POST['email'];
 
-    $title = "Оформление подписки на Best Tour Plan";
+    $title = "Subscription to Best Tour Plan";
     $body = "
-    <h2>Оформление подписки</h2>
-    <b>На маил:</b> $email
+    <h2>Subscription to Best Tour Plan</h2>
+    <b>For mail:</b> $email
     ";
     $mail = new PHPMailer\PHPMailer\PHPMailer();
     try {
@@ -22,7 +22,7 @@ if(isset($_POST["subs"])){
         
         $mail->Host       = 'daryadev.ru'; // SMTP сервера вашей почты
         $mail->Username   = 'besttourplan@daryadev.ru'; // Логин на почте
-        $mail->Password   = 'X2q3B4t4'; // Пароль на почте
+        $mail->Password   = 'I5r5L6b1'; // Пароль на почте
         $mail->SMTPSecure = 'ssl';
         $mail->Port       = 465;
         $mail->setFrom('besttourplan@daryadev.ru', 'Darya Selivanova'); // Адрес самой почты и имя отправителя
@@ -56,12 +56,12 @@ if(isset($_POST["mess"])){
     $message = $_POST['message'];
 
     // Формирование самого письма
-    $title = "Новое обращение Best Tour Plan";
+    $title = "Best Tour Plan new appeal";
     $body = "
-    <h2>Новое обращение</h2>
-    <b>Имя:</b> $name<br>
-    <b>Телефон:</b> $phone<br><br>
-    <b>Сообщение:</b><br> $message
+    <h2>Best Tour Plan new appeal</h2>
+    <b>Name:</b> $name<br>
+    <b>Phone:</b> $phone<br><br>
+    <b>Message:</b><br> $message
     ";
 
     // Настройки PHPMailer
@@ -76,7 +76,61 @@ if(isset($_POST["mess"])){
         
         $mail->Host       = 'daryadev.ru'; // SMTP сервера вашей почты
         $mail->Username   = 'besttourplan@daryadev.ru'; // Логин на почте
-        $mail->Password   = 'X2q3B4t4'; // Пароль на почте
+        $mail->Password   = 'I5r5L6b1'; // Пароль на почте
+        $mail->SMTPSecure = 'ssl';
+        $mail->Port       = 465;
+        $mail->setFrom('besttourplan@daryadev.ru', 'Darya Selivanova'); // Адрес самой почты и имя отправителя
+
+        // Получатель письма
+        $mail->addAddress('feral2015@mail.ru');  
+
+        // Отправка сообщения
+        $mail->isHTML(true);
+        $mail->Subject = $title;
+        $mail->Body = $body;    
+
+    // Проверяем отравленность сообщения
+    if ($mail->send()) {$result = "success";} 
+    else {$result = "error";}
+
+    } catch (Exception $e) {
+        $result = "error";
+        $status = "Сообщение не было отправлено. Причина ошибки: {$mail->ErrorInfo}";
+    }
+
+    // Отображение результата
+    header('Location: thankyou.html');
+}
+if(isset($_POST["messa"])){
+    // Переменные, которые отправляет пользователь
+    $name = $_POST['name'];
+    $phone = $_POST['phone'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+
+    // Формирование самого письма
+    $title = "Best Tour Plan new appeal";
+    $body = "
+    <h2>Best Tour Plan new appeal</h2>
+    <b>Name:</b> $name<br>
+    <b>Phone:</b> $phone<br><br>
+    <b>Email:</b> $email<br><br>
+    <b>Message:</b><br> $message
+    ";
+
+    // Настройки PHPMailer
+    $mail = new PHPMailer\PHPMailer\PHPMailer();
+    try {
+        $mail->isSMTP();   
+        $mail->CharSet = "UTF-8";
+        $mail->SMTPAuth = true;
+        $mail->SMTPDebug = 2;
+        $mail->Debugoutput = function($str, $level) {$GLOBALS['status'][] = $str;};
+
+        
+        $mail->Host       = 'daryadev.ru'; // SMTP сервера вашей почты
+        $mail->Username   = 'besttourplan@daryadev.ru'; // Логин на почте
+        $mail->Password   = 'I5r5L6b1'; // Пароль на почте
         $mail->SMTPSecure = 'ssl';
         $mail->Port       = 465;
         $mail->setFrom('besttourplan@daryadev.ru', 'Darya Selivanova'); // Адрес самой почты и имя отправителя
