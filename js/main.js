c72013cd8837b15ed680ac35dd1be27c6a1306dc
DaryaWebDev <feral2015@mail.ru>
2021-03-23 17:36:16 +0300

    added: animation

--- js/main.js
+++ js/main.js
@@ -189,10 +189,10 @@ $(document).ready(function () {
     });
   
   $(document).ready(function () {
-    $("input[type='tel']").mask('+7 (999) 999-9999');
-    $("input[type='tel']").mask('+7(999) 999-9999?');
+    $("input[type='tel']").mask('+7 (999) 999-99-99');
   });
   
+  AOS.init();
 
 });
 
